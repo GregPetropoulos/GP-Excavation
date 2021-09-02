3a836f6fa09460fac22bd08c0804c496839a872a
GP <gregpetropoulos@yahoo.com>
2021-09-02 12:52:25 -0400

    added a styled submit button for the news letter and will set up functionality on next commit, also made the footer responsive.

--- src/component/EmailForm.js
+++ src/component/EmailForm.js
@@ -1,27 +1,20 @@
-import React from 'react'
-import {Button, Form} from 'react-bootstrap'
+import React from 'react';
+import { Col, Row } from 'react-bootstrap';
 // Will need to get the email to be sent to db or gpc
 
-// need useState, handleclick, handlesubmit, validation, possible redirect
-const handleSubmit =() =>{
-  ''
-}
+const EmailForm = () => {
+  return (
+    <form>
+      <label>Email</label>
+      <input
+        className='form-email'
+        type='email'
+        name='email'
+        placeholder='email'
+      />
+      <button className='newsletter-button' type='submit' value= 'send'>Submit</button>
+    </form>
+  );
+};
 
-const EmailForm = (props) => {
-    return (
-        <Form onSubmit={handleSubmit}>
-        <Form.Group className="mb-3" controlId="formBasicEmail">
-          <Form.Label>Email address</Form.Label>
-          <Form.Control type="email" placeholder="Enter email" />
-          <Form.Text className="text-muted">
-            We'll never share your email with anyone else.
-          </Form.Text>
-        </Form.Group>
-        <Button variant="primary" type="submit">
-    Submit
-  </Button>
-</Form>
-    )
-}
-
-export default EmailForm
+export default EmailForm;
--- src/component/Footer.js
+++ src/component/Footer.js
@@ -1,48 +1,71 @@
 import React from 'react';
 import { Row, Col, Container } from 'react-bootstrap';
 import EmailForm from './EmailForm';
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
+import styled from 'styled-components';
 
 const Footer = () => {
+  const FooterResponsive = styled.div`
+    @media (max-width: 585px) {
+      .footer-wrapper {
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+      }
+      .footer-about-wrapper{
+        border-right:#ffc500
+      }
+      .footer-contact-wrapper{
+        border-right:#ffc500;
+        // display:flex;
+// flex-direction:row;
+// justify-content:center;
+
+      }
+    }
+  `;
   return (
     <Container>
-      <Row className='footer-wrapper'>
-        <Col className='footer-about-wrapper'>
-          <h1>About</h1>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
-            eiusmod tempor incididunt ut labore dolore magna aliqua.
-          </p>
-        </Col>
-          <Col className='footer-contact-wrapper'>
+      <FooterResponsive>
+        <Row className='footer-wrapper'>
+          <Col className='footer-about-wrapper'>
+            <h1>About</h1>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
+              eiusmod tempor incididunt ut labore dolore magna aliqua.
+            </p>
+          </Col>
+          <Col className='footer-contact-wrapper' xs={6}>
             <h1>Contact Us</h1>
             <p> 1234 Main St. Spring Valley Lake, CA 12345</p>
             <div className='email-phone-icon-wrapper'>
-
-            <i className='fas fa-envelope-square fa-2x'></i>
+              <i className='fas fa-envelope-square fa-2x'></i>
               <a
-              className='footer-email-link'
+                className='footer-email-link'
                 href='mailto: gpetroconstruction@gmail.com'
                 target='_blank'
                 rel='noopener noreferrer'
-                >
+              >
                 gpetroconstruction@gmail.com
               </a>
-
-            <i className='fas fa-mobile-alt fa-2x'></i>760-605-2024
-                </div>
-                <Col className='quote-col'>
-          <Link to='/quote'>
-            <button className='free-quote-btn-global'>Custom Quote</button>
-          </Link>
+              <i className='fas fa-mobile-alt fa-2x'></i>760-605-2024
+            </div>
+            <Col className='quote-col'>
+              <Link to='/quote'>
+                <button className='free-quote-btn-global'>Custom Quote</button>
+              </Link>
+            </Col>
           </Col>
+          <Col className='footer-newsletter-wrapper'>
+            <h1>Newsletter</h1>
+            <p>
+              You can trust us. we only send promo offers, not a single spam.
+            </p>
+            <EmailForm />
           </Col>
-        <Col className='footer-newsletter-wrapper'>
-          <h1>Newsletter</h1>
-          <p>You can trust us. we only send promo offers, not a single spam.</p>
-          <EmailForm/>
-        </Col>
-      </Row>
+        </Row>
+      </FooterResponsive>
     </Container>
   );
 };
