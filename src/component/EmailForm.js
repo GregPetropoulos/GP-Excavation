2c64c2e8d903a5eb37665499b66a8144cce6ed4f
GP <gregpetropoulos@yahoo.com>
2021-09-03 08:04:59 -0400

    removed extra console.log.

--- src/component/EmailForm.js
+++ src/component/EmailForm.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import emailjs from 'emailjs-com';
 import { Col, Row } from 'react-bootstrap';
-// Will need to get the email to be sent to db or gpc
-console.log('NEWSLETTER ID CHECK',process.env.REACT_APP_YOUR_NEWS_TEMPLATE_ID)
+
+
 const EmailForm = () => {
   function sendEmail(e) {
     e.preventDefault();
@@ -23,30 +23,32 @@ const EmailForm = () => {
           alert('An error occurred, Please try again');
           console.log(error.text);
         }
-        );
-        
-        e.target.reset();
-      }
+      );
+
+    e.target.reset();
+  }
   return (
-    <form  onSubmit={sendEmail}>
-        <Row >
-            <label htmlFor='email'>
-              Email:
-              <Col>
-                <input
-                  className='form-email'
-                  type='email'
-                  name='email'
-                  placeholder='email'
-                  required
-                />
-              </Col>
-            </label>
-            <Row className='form-btn-row'>
-          <button className='newsletter-button' type='submit' value='Send'>Submit</button>
+    <form onSubmit={sendEmail}>
+      <Row>
+        <label htmlFor='email'>
+          Email:
+          <Col>
+            <input
+              className='form-email'
+              type='email'
+              name='email'
+              placeholder='email'
+              required
+            />
+          </Col>
+        </label>
+        <Row className='form-btn-row'>
+          <button className='newsletter-button' type='submit' value='Send'>
+            Submit
+          </button>
         </Row>
-          </Row>  
-          </form>  
+      </Row>
+    </form>
   );
 };
 
