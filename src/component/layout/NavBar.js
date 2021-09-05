3b5b84d42e45603bddf6954ab3d68fd186e865e8
GP <gregpetropoulos@yahoo.com>
2021-09-04 21:40:08 -0400

    created a social icon component at nav bar.

--- src/component/layout/NavBar.js
+++ src/component/layout/NavBar.js
@@ -1,11 +1,12 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
-import { Container, Row, Col, Nav } from 'react-bootstrap';
+import { Container, Row, Col } from 'react-bootstrap';
 import styled from 'styled-components';
 
 // Will need to add media query  for md and sm in separate style sheet or inline
 import { Spin as Hamburger } from 'hamburger-react';
 import { MobileMenu } from '../MobileMenu';
+import SocialIcons from '../SocialIcons';
 
 const NavBar = () => {
   const [menuVisible, setMenuVisible] = useState(false);
@@ -30,97 +31,72 @@ const NavBar = () => {
   };
 
   return (
-    <>
-      <Container className='nav-container'>
-        <Row>
-          <Col className='nav-quote-btn'>
-            <Link to='/quote'>
-              <button className='quote-btn'>Free Quote</button>
-            </Link>
-          </Col>
-          <Col className='follow-heading'>
-            <h3>Follow us</h3>
-          </Col>
-          <Col className='navbar-icons'>
-            <a href='123' target='_blank' rel='noreferrer'>
-              <i className='fab fa-instagram'></i>
-            </a>
-            <a
-              href='https://www.facebook.com/GPExcavation-103086617779983/?ref=py_c'
-              target='_blank'
-              rel='noreferrer'
-            >
-              <i className='fab fa-facebook-f'></i>
-            </a>
-            <a href='123' target='_blank' rel='noreferrer'>
-              <i className='fab fa-twitter'></i>
-            </a>
-            <a href='123' target='_blank' rel='noreferrer'>
-              <i className='fab fa-linkedin'></i>
-            </a>
-          </Col>
-        </Row>
-
-        <Row>
-          <Col>
-            <div>
-              <i className='fas fa-hard-hat fa-3x'></i>{' '}
-              <span className=' logo'>GP Excavation</span>
-            </div>
-            <HideDesktopNavBar>
-              <nav>
-                <ul className='link-items'>
-                  <li>
-                    <Link to='/home' className='desktop-links'>
-                      Home
-                    </Link>
-                  </li>
-                  <li>
-                    <Link to='/about' className='desktop-links'>
-                      About
-                    </Link>
-                  </li>
-                  <li>
-                    <Link to='/service' className='desktop-links'>
-                      Services
-                    </Link>
-                  </li>
-                  <li>
-                    <Link to='/projects' className='desktop-links'>
-                      Projects
-                    </Link>
-                  </li>
-                  <li>
-                    <Link to='/blog' className='desktop-links'>
-                      Blog
-                    </Link>
-                  </li>
-                  <li>
-                    <Link to='/contact' className='desktop-links'>
-                      Contact
-                    </Link>
-                  </li>
-                </ul>
-                {/* new hamburger */}
-              </nav>
-            </HideDesktopNavBar>
-          </Col>
-        </Row>
-        <ResponsiveMenu>
-          <Hamburger
-            toggled={isOpen}
-            toggle={setOpen}
-            label='Show menu'
-            onClick={() => setOpen(!isOpen)}
-            color='whitesmoke'
-            size={40}
-            className='hamburger'
-          />
-          <div className='hamburger-spacer'></div>
-          {isOpen ? <MobileMenu onClick={closeMenu} /> : ''}
-        </ResponsiveMenu>
-      </Container>
-    </>
+    <Container className='nav-container'>
+      <Col className='nav-quote-btn'>
+        <Link to='/quote'>
+          <button className='quote-btn'>Free Quote</button>
+        </Link>
+      </Col>
+      <SocialIcons />
+      <Row>
+        <Col>
+          <div>
+            <i className='fas fa-hard-hat fa-3x'></i>{' '}
+            <span className=' logo'>GP Excavation</span>
+          </div>
+          <HideDesktopNavBar>
+            <nav>
+              <ul className='link-items'>
+                <li>
+                  <Link to='/home' className='desktop-links'>
+                    Home
+                  </Link>
+                </li>
+                <li>
+                  <Link to='/about' className='desktop-links'>
+                    About
+                  </Link>
+                </li>
+                <li>
+                  <Link to='/service' className='desktop-links'>
+                    Services
+                  </Link>
+                </li>
+                <li>
+                  <Link to='/projects' className='desktop-links'>
+                    Projects
+                  </Link>
+                </li>
+                <li>
+                  <Link to='/blog' className='desktop-links'>
+                    Blog
+                  </Link>
+                </li>
+                <li>
+                  <Link to='/contact' className='desktop-links'>
+                    Contact
+                  </Link>
+                </li>
+              </ul>
+              {/* new hamburger */}
+            </nav>
+          </HideDesktopNavBar>
+        </Col>
+      </Row>
+      <ResponsiveMenu>
+        <Hamburger
+          toggled={isOpen}
+          toggle={setOpen}
+          label='Show menu'
+          onClick={() => setOpen(!isOpen)}
+          color='whitesmoke'
+          size={40}
+          className='hamburger'
+        />
+        <div className='hamburger-spacer'></div>
+        {isOpen ? <MobileMenu onClick={closeMenu} /> : ''}
+      </ResponsiveMenu>
+    </Container>
   );
 };
 export default NavBar;
