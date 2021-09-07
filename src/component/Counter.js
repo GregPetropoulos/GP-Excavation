78a7c3a3396aa60489ef1b8c208a6074503beb6e
GP <gregpetropoulos@yahoo.com>
2021-09-07 07:33:30 -0400

    counter component is now mobile responsive.

--- src/component/Counter.js
+++ src/component/Counter.js
@@ -1,8 +1,33 @@
-import React, { useState } from 'react';
+// import React, { useState } from 'react';
 import { useSpring, animated, config } from 'react-spring';
 import { Row, Col, Container } from 'react-bootstrap';
+import styled from 'styled-components';
 
 export default function Counter() {
+  const ResponsiveCounter = styled.div`
+  @media (max-width:768px){
+    width:100%;
+    display:flex;
+    justify-content:center;
+    align-items:center;
+    flex-direction:column;
+    
+    .counter-wrapper {
+      display-flex;
+      justify-content:center;
+      align-items:center;
+      flex-direction:column;
+      width:100%;
+    }
+    .counter-items{
+      font-size:2.2rem;
+    }
+    .counter-row{
+      text-align: center;
+    }
+
+  }
+  `;
   const customerProps = useSpring({
     val: 180,
     from: { val: 0 },
@@ -28,53 +53,50 @@ export default function Counter() {
   });
 
   return (
-    <Container className='counter-wrapper'>
-      {/* <animated.div className='counter'>
-        {props.val.to((val) => Math.floor(val))}
-        </animated.div>
-    <h3>state practice</h3> */}
-
-      <Row>
-        <Col>
-          <animated.div className=' counter-items'>
-            {customerProps.val.to((val) => Math.floor(val))}
-          </animated.div>
-        </Col>
-        <Col>
+    <ResponsiveCounter>
+      <Container className='counter-wrapper'>
+        <Row className='counter-row'>
+          <Col>
+            <animated.div className=' counter-items'>
+              {customerProps.val.to((val) => Math.floor(val))}
+            </animated.div>
+          </Col>
+          {/* <Col> */}
           <h3 className='counter-title'>Number of Happy Customers</h3>
-        </Col>
-      </Row>
-      <Row>
-        <Col>
-          <animated.div className=' counter-items'>
-            {resProps.val.to((val) => Math.floor(val))}
-          </animated.div>
-        </Col>
-        <Col></Col>
-        <Col>
+          {/* </Col> */}
+        </Row>
+        <Row className='counter-row'>
+          <Col>
+            <animated.div className=' counter-items'>
+              {resProps.val.to((val) => Math.floor(val))}
+            </animated.div>
+          </Col>
+          <Col></Col>
+          {/* <Col> */}
           <h3 className='counter-title'>Number of Residential Projects</h3>
-        </Col>
-      </Row>
-      <Row>
-        <Col>
-          <animated.div className=' counter-items'>
-            {commercialProps.val.to((val) => Math.floor(val))}
-          </animated.div>
-        </Col>
-        <Col>
-          <h3 className='counter-title'>Number of Commercial Projects</h3>
-        </Col>
-      </Row>
-      <Row>
-        <Col>
-          <animated.div className=' counter-items'>
-            {coffeeProps.val.to((val) => Math.floor(val))}
-          </animated.div>
-        </Col>
-        <Col>
-          <h3 className='counter-title'>Cups of Coffee</h3>
-        </Col>
-      </Row>
-    </Container>
+          {/* </Col> */}
+        </Row>
+        <Row className='counter-row'>
+          <Col>
+            <animated.div className=' counter-items'>
+              {commercialProps.val.to((val) => Math.floor(val))}
+            </animated.div>
+          </Col>
+          <Col>
+            <h3 className='counter-title'>Number of Commercial Projects</h3>
+          </Col>
+        </Row>
+        <Row className='counter-row'>
+          <Col>
+            <animated.div className=' counter-items'>
+              {coffeeProps.val.to((val) => Math.floor(val))}
+            </animated.div>
+          </Col>
+          <Col>
+            <h3 className='counter-title'>Cups of Coffee</h3>
+          </Col>
+        </Row>
+      </Container>
+    </ResponsiveCounter>
   );
 }
